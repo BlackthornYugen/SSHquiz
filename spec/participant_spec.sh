5381c677e1c4c727a01ed23045a758cedfb05c8c
John Steel <john@jskw.dev>
2023-04-09 20:02:03 +0000

    fix inconsistant indentation

--- spec/participant_spec.sh
+++ spec/participant_spec.sh
@@ -19,21 +19,21 @@ Describe "Participant Script"
   It "asks for your handle"
     When call quiz
     The status should be success
-      The output line "4" should include "Enter your handle (up to 7 letters, no spaces or special characters)--> TestUser"
-      The status should be success
+    The output line "4" should include "Enter your handle (up to 7 letters, no spaces or special characters)--> TestUser"
+    The status should be success
   End
 
   It "congratulate users for finishing the quiz"
     When call quiz
     The status should be success
-      The output line "33" should include "CONGRATULATIONS! You have successfully completed this quiz."
-      The status should be success
+    The output line "33" should include "CONGRATULATIONS! You have successfully completed this quiz."
+    The status should be success
   End
 
   It "should refer people to fedora"
     When call quiz
     The status should be success
-      The output line "34" should include "https://getfedora.org"
-      The status should be success
+    The output line "34" should include "https://getfedora.org"
+    The status should be success
   End
 End
